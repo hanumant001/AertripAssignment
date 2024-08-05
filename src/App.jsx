e4b2c8ae764f18fbae32971bc6bb29c1966463d0
hanumant001 <hanumanthanchinamani@gmail.com>
2024-08-05 23:08:29 +0530

    AertripAssignment

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ function App() {
       <main className="pt-16 p-4">
         <SearchInput onSearch={handleSearch} />
         <SortOptions onSortChange={handleSortChange} />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
           {sortedFlights.map((flight) => (
             <FlightCard key={flight.id} flight={flight} />
           ))}
--- src/components/FlightCard.jsx
+++ src/components/FlightCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FlightCard = ({ flight }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-4 mb-4 transition-transform transform hover:scale-105">
+    <div className="bg-white  shadow-md rounded-lg p-4 mb-4 transition-transform transform hover:scale-105 cardFlights">
       <h2 className="text-xl font-bold mb-2">{flight.fr} to {flight.to}</h2>
       <p className="text-gray-600">Departure: {flight.dep_date}</p>
       <p className="text-gray-600">Arrival: {flight.arr_date}</p>
